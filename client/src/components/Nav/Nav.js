fad6988c2948afd233c3a15cdc6d1b878476c919
chantelle mckenzie <mrspasceri@hotmail.com>
2022-07-20 20:15:57 -0700

    first commit

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,39 @@
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import "./Nav.scss";
+import logo from "../../assets/logo/logo.png";
+
+export default function Header() {
+  return (
+    <div className="nav">
+      <Navbar bg="transparent" expand="lg">
+        <Container className="nav-container">
+          <Navbar.Brand href="/">
+            <img
+              className="nav-logo d-inline-block align-middle mr-2"
+              width="200"
+              alt="logo-img"
+              src={logo}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="nav-bar">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/about" className="nav-bar">
+                About
+              </Nav.Link>
+              <Nav.Link href="/projects" className="nav-bar">
+                Projects
+              </Nav.Link>
+              <Nav.Link href="/contact" className="nav-bar">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
