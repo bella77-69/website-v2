e2feed8b32cd6854e7ad57e9f2d18bb25b8d8ee8
chantelle mckenzie <mrspasceri@hotmail.com>
2022-07-27 19:34:30 -0700

    Server folder: installed body-parser, cors, dotenv, and mysql.  Set up server connection

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.status(200).json('Hello');
+})
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server connected to port: ${PORT}`);
+});
