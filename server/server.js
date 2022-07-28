7ef44fd33ed19e0acaa7d40b75434ded2d8b7591
chantelle mckenzie <mrspasceri@hotmail.com>
2022-07-27 19:54:27 -0700

    Revert "Server folder: set up mysql schema for website comments, set up routes."
    This reverts commit ad230986bdf8bed496457e8e1ccf7b0538876c50.

--- server/server.js
+++ server/server.js
@@ -5,13 +5,11 @@ const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
-const commentRoute = require('./routes/commentsRoute')
 
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('./website/comments', commentRoute);
 
 const PORT = process.env.PORT || 8000;
 
