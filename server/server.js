683b79d7846197200191f764e9b0855dcf0b59b9
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-10 20:37:21 -0700

    first heroku git commit

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 app.use("/api", commentRoute);
 app.get("/", (req, res) => {
   res.status(200).json("Hello");
