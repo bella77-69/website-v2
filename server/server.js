1cde762040b3a67829be21d5133217aec26bc90c
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-15 18:24:33 -0700

    test

--- server/server.js
+++ server/server.js
@@ -16,19 +16,19 @@ app.use("/api", commentRoute);
 
 const PORT = process.env.PORT || 8000;
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 // });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`);
