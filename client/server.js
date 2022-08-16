ac985ba6e8e9a10682bd50daa18415f3d4cad62e
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-15 19:54:02 -0700

    test

--- client/server.js
+++ client/server.js
@@ -16,14 +16,14 @@ app.use("/api", commentRoute);
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname + "/public")))
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+// app.use(express.static(path.join(__dirname + "/public")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`);
