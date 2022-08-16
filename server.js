b37ffa0341574e71d01d83d55228d50f726e3f5c
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-16 15:27:52 -0700

    updated scripts in Package.json for server

--- server.js
+++ server.js
@@ -14,15 +14,21 @@ app.use(bodyParser.json());
 
 app.use("/api", commentRoute);
 
+app.use(express.static(path.resolve(__dirname, './client/build')))
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+})
+
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
 
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`);
