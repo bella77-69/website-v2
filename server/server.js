b21c5ad9ae967bfee11879a51e65abd0c0444974
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-15 19:37:05 -0700

    created Server folder and moved files back into folder

--- server/server.js
+++ server/server.js
@@ -16,19 +16,14 @@ app.use("/api", commentRoute);
 
 const PORT = process.env.PORT || 8000;
 
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname + "/public")))
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
 
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`);
