da7d62ba5a462d9d3ca1798df1f48656fb747cf0
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-16 20:21:20 -0700

    testing again

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ require("dotenv").config();
 const app = express();
 const commentRoute = require("./routes/commentsRoute");
 const path = require("path");
-// const serveStatic = require('serve-static');
+const serveStatic = require('serve-static');
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(serveStatic(__dirname + '/client/build'));
+app.use(serveStatic(__dirname + '/build'));
 app.use("/api", commentRoute);
 
 
 
-app.use(express.static(path.join(__dirname, './build')))
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build", "index.html"))
-})
+// app.use(express.static(path.join(__dirname, './build')))
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./build", "index.html"))
+// })
 
 // // Other app.use middleware
 // app.use(express.static(path.join(__dirname, "client", "build")));
