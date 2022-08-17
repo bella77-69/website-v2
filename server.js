f9e8f58779366c32627e3b68d374501cce8b7029
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-17 14:39:33 -0700

    test

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (req, res) => {
