dc9c71ed00c4fdd2c291e044b91c1ad913a5d13f
chantelle mckenzie <mrspasceri@hotmail.com>
2022-08-17 19:54:11 -0700

    test

--- server.js
+++ server.js
@@ -22,13 +22,20 @@ app.use("/api", commentRoute);
 // });
  
 
-if (process.env.NODE_ENV == "production") {
-  app.use(serveStatic(__dirname + '/build'));
+// if (process.env.NODE_ENV == "production") {
+//   app.use(serveStatic(__dirname + '/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(__dirname + '/../static/index.html');
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(__dirname + '/../static/index.html');
+//   });
+// }
+
+if (process.env.NODE_ENV === 'production') {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+})
+};
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, (req, res) => {
