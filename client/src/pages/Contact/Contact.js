9bc38e0fa70998b2f025c3c8ce907ae14cfa34e6
chantelle mckenzie <mrspasceri@hotmail.com>
2022-12-26 19:43:03 -0800

    changed api call in Contact.js file

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -28,7 +28,7 @@ export default function Contact() {
     };
 
     axios
-    .post("http://localhost:8000/api", data)
+    .post("/api", data)
       .then((response) => {
         if (response.status === 200) {
           setComment((prevState) => ({
