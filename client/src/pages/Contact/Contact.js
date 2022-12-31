12ed81e9eddb9c3a7399cd91c280f9841e140545
chantelle mckenzie <mrspasceri@hotmail.com>
2022-12-30 22:10:20 -0800

    updated post request in Contact.js file

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -28,7 +28,7 @@ export default function Contact(props) {
     };
 
     axios
-      .post("http://localhost:8000/api", data)
+      .post("https://63afd137f6d836000962529a--chantelle-website-v2.netlify.app/api", data)
       .then((response) => {
         if (response.status === 200) {
           setComment((prevState) => ({
