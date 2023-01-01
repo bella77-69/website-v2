2a1160e2eccda9635ab85fdfd20b45ebb80272d1
chantelle mckenzie <mrspasceri@hotmail.com>
2023-01-01 09:10:29 -0800

    installed concurrently to run both client and server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const commentRoute = require("./routes/commentsRoute");
-const path = require('path');
 const app = express();
 
 
@@ -12,17 +11,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-app.use(express.static(path.join(__dirname, 'build')));
 app.use("/api", commentRoute);
 
-app.get('/', function (req, res) {
-  +app.get('/*', function (req, res) {
-     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-   });
-  }); 
-  
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, (req, res) => {
