3b8f957f8a933e1c156fe5bc2a65d3bc5b105ab4
chantelle mckenzie <mrspasceri@hotmail.com>
2023-01-01 09:32:26 -0800

    api call back to /api

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -28,7 +28,7 @@ export default function Contact(props) {
     };
 
     axios
-      .post("https://chantelle-website-v2.netlify.app/api", data)
+      .post("/api", data)
       .then((response) => {
         if (response.status === 200) {
           setComment((prevState) => ({
