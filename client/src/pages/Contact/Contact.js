ce4a9b0bd01483bfd6884737aaa40308f9dc92a4
chantelle mckenzie <mrspasceri@hotmail.com>
2023-01-01 08:10:46 -0800

    changed api call back to local host

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -28,7 +28,7 @@ export default function Contact(props) {
     };
 
     axios
-      .post("https://chantelle-website-v2.netlify.app/api", data)
+      .post("http//localhost:8000/api", data)
       .then((response) => {
         if (response.status === 200) {
           setComment((prevState) => ({
