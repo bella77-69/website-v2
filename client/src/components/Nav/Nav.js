8912cfc21c33fb668c23de7cb44fbdafef3c6dc6
chantelle mckenzie <mrspasceri@hotmail.com>
2023-09-07 16:18:01 -0700

    added animations to the Nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,36 +1,39 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container} from "react-bootstrap";
 import "./Nav.scss";
 import logo from "../../assets/logo/logo.jpg";
 
 export default function Header() {
+
   return (
-    <div className="nav">
-      <Navbar expand="lg">
+<div className="nav">
+      <Navbar bg="transparent" expand="lg">
         <Container className="nav-container">
-          <Navbar.Brand href="/" className="nav-logo">
+          <Navbar.Brand href="/" className="nav-container__box">
             <img
-              className="d-inline-block align-middle"
+              className="nav-logo d-inline-block align-middle"
               width="200"
-              height="auto"
               alt="logo-img"
               src={logo}
             />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/" className="nav-link">
-              Home
-            </Nav.Link>
-            <Nav.Link href="/about" className="nav-link">
-              About
-            </Nav.Link>
-            <Nav.Link href="/projects" className="nav-link">
-              Projects
-            </Nav.Link>
-            <Nav.Link href="/contact" className="nav-link">
-              Contact
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="nav-bar">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/about" className="nav-bar">
+                About
+              </Nav.Link>
+              <Nav.Link href="/projects" className="nav-bar">
+                Projects
+              </Nav.Link>
+              <Nav.Link href="/contact" className="nav-bar">
+               Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
