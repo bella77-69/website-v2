b086f6e9d9bbdd65f5ba4d651b89c5448e82e031
chantelle mckenzie <mrspasceri@hotmail.com>
2023-09-07 15:31:51 -0700

    refractored Footer.js, added a scroll to top button

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.scss";
 import { FaLinkedinIn, FaGithub, FaTwitter } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import logosmall from "../../assets/logo/logo-sm.png";
+import { BsArrowUp } from "react-icons/bs";
 
-export default function footer() {
+export default function Footer() {
+  const [showToTopButton, setShowToTopButton] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) {
+      setShowToTopButton(true);
+    } else {
+      setShowToTopButton(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
     <footer className="footer">
       <div className="footer-container">
@@ -20,7 +40,7 @@ export default function footer() {
               <li>
                 {" "}
                 <a href="mailto:mrspascer@hotmail.com" className="footer-icon">
-                  <HiOutlineMail className="footer-svg"/>
+                  <HiOutlineMail className="footer-svg" />
                 </a>
               </li>
               <li>
@@ -29,24 +49,29 @@ export default function footer() {
                   href="https://www.linkedin.com/in/chantellepasceri/"
                   className="footer-icon"
                 >
-                  <FaLinkedinIn className="footer-svg"/>
+                  <FaLinkedinIn className="footer-svg" />
                 </a>
               </li>
               <li>
                 {" "}
                 <a href="https://twitter.com/home" className="footer-icon">
-                  <FaTwitter className="footer-svg"/>
+                  <FaTwitter className="footer-svg" />
                 </a>
               </li>
               <li>
                 {" "}
                 <a href="https://github.com/bella77-69" className="footer-icon">
-                  <FaGithub className="footer-svg"/>
+                  <FaGithub className="footer-svg" />
                 </a>
               </li>
             </ul>
           </div>
         </div>
+        {showToTopButton && (
+          <button className="scroll-to-top" onClick={handleScrollToTop}>
+            <BsArrowUp className="scroll-to-top-icon" />
+          </button>
+        )}
       </div>
     </footer>
   );
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,28 +1,9 @@
-// import React from "react";
-// import "./home.scss";
-
-// function Home() {
-//   return (
-//     <header>
-//       <div className="box">
-//         <h1>- I am Chantelle -</h1>
-//         <h3>Full-Stack Web Developer</h3>
-//         <h4>
-//           Building state-of-the-art, easy to use, user-friendly websites and
-//           applications is truly a passion of mine!
-//         </h4>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Home;
 import React from "react";
 import "./home.scss";
 
 function Home() {
   return (
-    <header>
+    <header className="home">
       <div className="box">
         <h1>- I am Chantelle -</h1>
         <h3>Full-Stack Web Developer</h3>
