44a92cd8d45701d8e35bb7c5e0ac0c1ae34ff177
chantelle mckenzie <mrspasceri@hotmail.com>
2023-09-07 15:04:19 -0700

    created a new logo, refractored the Navigation

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,37 +1,36 @@
+import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Nav.scss";
-import logo from "../../assets/logo/logo.png";
+import logo from "../../assets/logo/logo.jpg";
 
 export default function Header() {
   return (
     <div className="nav">
-      <Navbar bg="transparent" expand="lg">
+      <Navbar expand="lg">
         <Container className="nav-container">
-          <Navbar.Brand href="/" className="nav-container__box">
+          <Navbar.Brand href="/" className="nav-logo">
             <img
-              className="nav-logo d-inline-block align-middle"
+              className="d-inline-block align-middle"
               width="200"
+              height="auto"
               alt="logo-img"
               src={logo}
             />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/" className="nav-bar">
-                Home
-              </Nav.Link>
-              <Nav.Link href="/about" className="nav-bar">
-                About
-              </Nav.Link>
-              <Nav.Link href="/projects" className="nav-bar">
-                Projects
-              </Nav.Link>
-              <Nav.Link href="/contact" className="nav-bar">
-               Contact
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link href="/" className="nav-link">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/about" className="nav-link">
+              About
+            </Nav.Link>
+            <Nav.Link href="/projects" className="nav-link">
+              Projects
+            </Nav.Link>
+            <Nav.Link href="/contact" className="nav-link">
+              Contact
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
     </div>
