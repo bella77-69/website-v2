201ddad73b0fc39b72b3e519a82c86e1b97da89d
chantelle mckenzie <mrspasceri@hotmail.com>
2023-09-08 10:09:18 -0700

    new images

--- client/src/components/ProjectData/WebDev.js
+++ client/src/components/ProjectData/WebDev.js
@@ -1,3 +1,5 @@
+import home from '../../assets/projects/homePageDesktop.jpg';
+import home2 from '../../assets/projects/homePageMobile.jpg';
 import lash from "../../assets/projects/lash.jpg";
 import lash2 from "../../assets/projects/lash1.jpg";
 import movie from "../../assets/projects/movie.jpg";
@@ -9,6 +11,18 @@ import coffee2 from "../../assets/projects/coffee1.png";
 export const PrjData = [
   {
     id: 1,
+    name: "Acknowledging Our Lands",
+    github: "https://github.com/Bella77-69/land-acknowledgement",
+    website: "",
+    desc: "Acknowledging Our Lands is a web application that helps users learn about and perform land acknowledgments. It provides a simple and educational platform for acknowledging the Indigenous territories and traditional lands on which we live and work.",
+    desktopGif: home,
+    mobileGif: home2,
+    features: ["Land Acknowledgement - Easily generate land acknowledgments for various locations.",
+    "Learning - Learn about Indigenous territories and the importance of land acknowledgments.",],
+    techs: ["React","React-Router","Axios","Bootstrap",],
+  },
+  {
+    id: 2,
     name: "Bella Lashes Inc",
     github: "https://github.com/Bella77-69/lash-app",
     website: "",
@@ -19,7 +33,7 @@ export const PrjData = [
     techs: ["React","Mongo","Express","Node","Material-ui","React-Router","React-Redux","Axios","Bootstrap","Styled-Components","JsonWebToken","BCryptjs","Mongoose",],
   },
   {
-    id: 2,
+    id: 3,
     name: "Movie App",
     github: "https://github.com/bella77-69/movie-app2",
     website: "",
@@ -30,7 +44,7 @@ export const PrjData = [
     techs: ["React", "React-YouTube", "React-Router", "Sass", "Axios"],
   },
   {
-    id: 3,
+    id: 4,
     name: "Steamy Cup",
     github: "https://github.com/bella77-69/comp-1850-final-project",
     website: "",
